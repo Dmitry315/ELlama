64f5d0ec26ea8480358393261e38e524217a2b43
Дмитрий Мелихов <melikhov.dmitry.a@gmail.com>
2025-11-04 20:05:44 +0000

    fix scripts so we can run out of box

--- scripts/download_fineweb.sh
+++ scripts/download_fineweb.sh
@@ -1,4 +1,4 @@
 python src/data_process/prepare_fineweb.py \
-    --save-path "src/experiments/data/fineweb2_example" \
+    --save-path "src/experiments/example/data/fineweb2" \
     --to-txt \
-    --save-path-txt "src/experiments/data/fineweb2_example/train.txt"
+    --save-path-txt "src/experiments/example/data/fineweb2/train.txt"
